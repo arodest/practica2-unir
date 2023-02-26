d71e371788b908ad67793ee227f663ddfeccc099
Antonio Rodes <rodes32@gmail.com>
2023-02-26 19:34:59 +0100

    variables terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,15 @@
 #caso practico 2 - Antonio Rodes
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.10.0"
+    }
+  }
+
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
--- terraform/recursos.tf
+++ terraform/recursos.tf
@@ -1,1 +1,23 @@
 #caso practico 2 - Antonio Rodes
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "containerRegistry1"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Premium"
+  admin_enabled       = false
+  georeplications {
+    location                = "East US"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+  georeplications {
+    location                = "North Europe"
+    zone_redundancy_enabled = true
+    tags                    = {}
+  }
+}
