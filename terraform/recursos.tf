e4bba3c83012b1df962ab69d29f550e13b22e669
arodest <87528874+arodest@users.noreply.github.com>
2023-03-08 16:20:02 +0100

    Update recursos.tf

--- terraform/recursos.tf
+++ terraform/recursos.tf
@@ -1,13 +1,13 @@
 #caso practico 2 - Antonio Rodes
-resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+resource "azurerm_resource_group" "practica2" {
+  name     = "practica2-resources"
   location = "West Europe"
 }
 
 resource "azurerm_container_registry" "acr" {
   name                = "containerRegistry1"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.practica2.name
+  location            = azurerm_resource_group.practica2.location
   sku                 = "Premium"
   admin_enabled       = false
   georeplications {
